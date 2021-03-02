4adc97dbffb8317f6c0efd38fe67657485c52e4f
hnieto <hernan.nieto89@gmail.com>
2021-03-02 20:14:27 -0300

    wip

--- scheduler/utils/base.py
+++ scheduler/utils/base.py
@@ -54,7 +54,7 @@ def _parse_conditions(conditions):
                 sensor_id = splitted[1]
                 print(sensor_type, sensor_id, target_metric, connector, target_value)
                 sensor_value = _get_sensor_value(sensor_type, sensor_id, target_metric)
-                evaluated_condition = _evaluate_condition(sensor_value, connector, target_value)
+                evaluated_condition = _evaluate_condition(sensor_type, sensor_value, connector, target_value, target_metric)
                 result.append(evaluated_condition)
     if all(result):
         return "True"
@@ -62,7 +62,34 @@ def _parse_conditions(conditions):
         return "False"
 
 
-def _evaluate_condition(sensor_value, connector, target_value):
+def _evaluate_condition(sensor_type, sensor_value, connector, target_value, target_metric):
+    if sensor_type == "Clock":
+        return _evaluate_dates(sensor_value, connector, target_value, target_metric)
+    else:
+        return _evaluate_num(sensor_value, connector, target_value)
+
+
+def _evaluate_dates(sensor_value, connector, target_value, target_metric):
+
+    if connector == "BT":
+        start, end = target_value.split(".")
+        if target_metric = "H": start_date = {"hour": start}; end_date = {"hour": end}; sensor_date = {"hour": sensor_date}
+        if target_metric = "M": start_date = {"minute": start}; end_date = {"minute": end}; sensor_date = {"minute": sensor_date}
+        if target_metric = "S": start_date = {"second": start}; end_date = {"second": end}; sensor_date = {"second": sensor_date}
+        return _between_dates(start_date, end_date, sensor_value)
+
+    if target_metric = "H": target_date = {"hour": target_value}; sensor_date = {"hour": sensor_date}
+    if target_metric = "M": target_date = {"minute": target_value}; sensor_date = {"minute": sensor_date}
+    if target_metric = "S": target_date = {"second": target_value}; sensor_date = {"second": sensor_date}
+
+    if connector == "GT": return time(sensor_date) > time(**target_date)
+    elif connector == "GE": return time(sensor_date) >= time(**target_date)
+    elif connector == "LT": return time(sensor_date) < time(**target_date)
+    elif connector == "LE": return time(sensor_date) <= time(**target_date)
+    elif connector == "EQ": return time(sensor_date) == time(**target_date)
+
+
+def _evaluate_num(sensor_value, connector, target_value):
     if connector == "GT": return float(sensor_value) > float(target_value)
     elif connector == "GE": return float(sensor_value) >= float(target_value)
     elif connector == "LT": return float(sensor_value) < float(target_value)
@@ -74,3 +101,10 @@ def _get_sensor_value(sensor_type, sensor_id, sensor_metric):
     url = SENSOR_URL_TEMPLATE.format(sensor_type=sensor_type, sensor_id=sensor_id)
     response = requests.get(url).json()
     return response[sensor_metric]
+
+
+def _between_dates(start, end, value):
+    if begin_time < end_time:
+        return check_time >= start and check_time <= end
+    else: # crosses midnight
+        return check_time >= start or check_time <= end
