18302abfd01a02547363ffcec829d638e68a0034
hnieto <hernan.nieto89@gmail.com>
2021-03-05 19:13:10 -0300

    wip

--- scheduler/utils/base.py
+++ scheduler/utils/base.py
@@ -2,11 +2,10 @@ import requests
 import json
 import time
 import datetime
-import logging
-logging.basicConfig()
+
 
 VALID_BOOLEANS = ["True", "False"]
-VALID_CONNECTORS = ["GT", "LT", "EQ", "GE", "LE"]
+VALID_CONNECTORS = ["GT", "LT", "EQ", "GE", "LE", "BT"]
  
 SENSOR_TEMPLATE = "{sensor_type}-{sensor_id}-{sensor_metric}"
 
@@ -19,9 +18,9 @@ ACTION_URL_TEMPLATE = "http://localhost:5000/actions/{action_type}/{action_id}"
 
 def run(conditions, actions_dict, work_time, sleep_time, teardown_action):
     rule_value = _parse_conditions(conditions)
-    logging.info(f"RULE VALUE: {rule_value}")
+    print(f"RULE VALUE: {rule_value}")
     action_template = json.loads(actions_dict)[rule_value]
-    logging.info(f"ACTION TEMPLATE: {action_template}")
+    print(f"ACTION TEMPLATE: {action_template}")
     _perform_action(action_template)
     if rule_value == "True" and work_time:
         time.sleep(int(work_time))
@@ -45,18 +44,31 @@ def _parse_conditions(conditions):
     Returns True if all conditions al met. False otherwise.
     """
     result = []
+    print("parse")
+    print(conditions)
     for condition in json.loads(conditions):
         splitted = condition.split("-")
+        print("splited ", splitted )
         if len(splitted) == 1:
-            result.append(bool(condition))
+            print(splitted)
+            if splitted[0] == "True":
+                condition = True
+            else:
+                condition = False
+            result.append(condition)
         if len(splitted) == 3:
             target_metric, connector, target_value = splitted[2].split("_")
+            print("splitted 3")
+            print("target_metric, connector, target_value")
+            print(target_metric, connector, target_value)
             if connector in VALID_CONNECTORS:
-                logging.info("Sensor condition!")
+                print("Sensor condition!")
                 sensor_type = splitted[0]
                 sensor_id = splitted[1]
-                logging.info(f"{sensor_type}, {sensor_id}, {target_metric}, {connector}, {target_value}")
+                print(f"{sensor_type}, {sensor_id}, {target_metric}, {connector}, {target_value}")
                 sensor_value = _get_sensor_value(sensor_type, sensor_id, target_metric)
+                print("SENSOR VALUE")
+                print(sensor_value)
                 evaluated_condition = _evaluate_condition(sensor_type, sensor_value, connector, target_value, target_metric)
                 result.append(evaluated_condition)
     if all(result):
@@ -66,35 +78,35 @@ def _parse_conditions(conditions):
 
 
 def _evaluate_condition(sensor_type, sensor_value, connector, target_value, target_metric):
-    logger.logging(f"sensor type {sensor_type}")
-    logger.logging(f"sensor type {sensor_value}")
-    logger.logging(f"sensor type {connector}")
-    logger.logging(f"sensor type {target_value}")
-    logger.logging(f"sensor type {target_metric}")
+    print(f"sensor type {sensor_type}")
+    print(f"sensor value {sensor_value}")
+    print(f"sensor Connector {connector}")
+    print(f"Target value {target_value}")
+    print(f"Target_metrick {target_metric}")
 
     if sensor_type == "Clock":
-        logger.logging("EVALUATES CLOCK")
+        print("EVALUATES CLOCK")
         return _evaluate_dates(sensor_value, connector, target_value, target_metric)
     else:
-        logger.logging("EVALUATES NUM")
+        print("EVALUATES NUM")
         return _evaluate_num(sensor_value, connector, target_value)
 
 
 def _evaluate_dates(sensor_value, connector, target_value, target_metric):
     if connector == "BT" and target_metric == "H":
-        logger.logging("BT and H")
+        print("EVALUATEE BT")
         start, end = target_value.split(".")
         start_date = datetime.time(hour=int(start))
         end_date = datetime.time(hour=int(end))
         sensor_date = datetime.time(hour=int(sensor_value))
+        print(start_date, end_date, sensor_date)
         return _between_dates(start_date, end_date, sensor_date)
 
     if target_metric == "H": target_date = datetime.time(hour=int(target_value)); sensor_date = datetime.time(hour=int(sensor_value))
     if target_metric == "M": target_date = datetime.time(minute=int(target_value)); sensor_date = datetime.time(minute=int(sensor_value))
     if target_metric == "S": target_date = datetime.time(second=int(target_value)); sensor_date = datetime.time(second=int(sensor_value))
 
-    if connector == "BT": return sensor_date >= start_date and sensor_date <= end_date
-    elif connector == "GT": return sensor_date > target_date
+    if connector == "GT": return sensor_date > target_date
     elif connector == "GE": return sensor_date >= target_date
     elif connector == "LT": return sensor_date < target_date
     elif connector == "LE": return sensor_date <= target_date
--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,7 +1,10 @@
 import pytest
+import requests
+import requests_mock
+
 import datetime
 
-from scheduler.utils.base import _parse_conditions, _evaluate_condition, _evaluate_dates, _between_dates
+from scheduler.utils.base import _parse_conditions, _evaluate_dates, _between_dates
 
 
 @pytest.mark.parametrize("condition, expected", [('["True"]', "True"), ('["False"]', "False")])
@@ -15,3 +18,15 @@ def test__between_dates(start, end, current_value, expected):
     end_date = datetime.time(hour=end)
     target_date = datetime.time(hour=current_value)
     assert _between_dates(start_date, end_date, target_date) == expected
+
+
+@pytest.mark.parametrize("sensor_value, connector, target_value, target_metric, expected", [("18", "BT", "2.6", "H", False), ("18", "BT", "6.2", "H", True)])
+def test__evaluate_dates(sensor_value, connector, target_value, target_metric, expected):
+        assert _evaluate_dates(sensor_value, connector, target_value, target_metric) == expected
+
+
+@pytest.mark.parametrize("conditions, response, expected", [('["Clock-1-H_BT_2.6"]', '{"H": 18, "M": 30, "S": 30}', "False"), ('["Clock-1-H_BT_6.2"]', '{"H": 18, "M": 30, "S": 30}', "True")])
+def test__parse_conditions(conditions, response, expected):
+    with requests_mock.Mocker() as m:
+        m.get('http://localhost:5000/sensors/Clock/1', text=response)
+        assert _parse_conditions(conditions) == expected
